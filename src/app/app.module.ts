d6916cc55b1229b8dccc6ec6e61092b63ff4f04c
Medos777 <medaminegh50@gmail.com>
2020-08-11 02:17:48 +0200

    v2.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,70 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddClientComponent } from './client/add-client/add-client.component';
+import { ListClientComponent } from './client/list-client/list-client.component';
+import { AddCategorieComponent } from './categorie/add-categorie/add-categorie.component';
+import { ListCategorieComponent } from './categorie/list-categorie/list-categorie.component';
+import { AddScategorieComponent } from './scategorie/add-scategorie/add-scategorie.component';
+import { ListScategorieComponent } from './scategorie/list-scategorie/list-scategorie.component';
+import { AddArticleComponent } from './article/add-article/add-article.component';
+import { ListArticleComponent } from './article/list-article/list-article.component';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { NgMatSearchBarModule } from 'ng-mat-search-bar';
+import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { DatePipe } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
+import { RouterModule, Routes } from '@angular/router';
+import { ToastrModule } from 'ngx-toastr';
+import { HttpClientModule } from '@angular/common/http';
 
+
+
+const MATERIAL_MODULES = [MatToolbarModule,
+  MatIconModule
+];
+const appRoutes : Routes = [
+  {path: 'categories', component: ListCategorieComponent},
+  {path: 'categorie', component: AddCategorieComponent},
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddClientComponent,
+    ListClientComponent,
+    AddCategorieComponent,
+    ListCategorieComponent,
+    AddScategorieComponent,
+    ListScategorieComponent,
+    AddArticleComponent,
+    ListArticleComponent?
+    MatIconModule?
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    ReactiveFormsModule,
+    FormsModule,
+    ToastrModule.forRoot(),
+    HttpClientModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    
+    
+    MatSliderModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    NgMatSearchBarModule,
   ],
-  providers: [],
+  exports : MATERIAL_MODULES,
+  providers: [DatePipe,{ provide: MAT_DIALOG_DATA, useValue: {} ,},
+    { provide: MatDialogRef, useValue: {} }],
+  
   bootstrap: [AppComponent]
 })
 export class AppModule { }
